73170ab1a2c89114b4638454f3efc5440f7fc10a
Christopher Davis <chris.j.davis@duke.edu>
2020-10-22 15:25:25 -0400

    Two Masks

--- test.py
+++ test.py
@@ -22,19 +22,21 @@ faces = face_cascade.detectMultiScale(gray, 1.1, 10)
 
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+   # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+   print(1)
 
 # Display the output
-cv2.imshow('img', img)
 cv2.waitKey()
 
 # Change BGR to RGB
 img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+anewh = int(h/2)
+anewy = int(y+anewh)
 
 # Make the Mask
 mask = np.zeros(img2.shape[:2], np.uint8)
 mask[:,:] = 0
-rectangle = cv2.rectangle(mask,(x,y),(x+w,y+h),(255,255,255),-1)
+rectangle = cv2.rectangle(mask,(x,y),(x+w,y+(anewh)),(255,255,255),-1)
 masked_img = cv2.bitwise_and(img2,img2,mask = rectangle) #mask
 
 # Calculate histogram with mask and without mask
@@ -42,13 +44,29 @@ masked_img = cv2.bitwise_and(img2,img2,mask = rectangle) #mask
 color = ('r','g','b')
 for i,col in enumerate(color):
     histr = cv2.calcHist([img2],[i],mask,[256],[0,256])
-    plt.subplot(224), plt.plot(histr,color = col)
+    plt.subplot(324), plt.plot(histr,color = col)
     plt.xlim([0,256])
+    
+    
+    
+mask2 = np.zeros(img2.shape[:2], np.uint8)
+mask2[:,:] = 0
+rectangle2 = cv2.rectangle(mask2,(x,anewy),(x+w,y+h),(255,255,255),-1)
+masked_img2 = cv2.bitwise_and(img2,img2,mask = rectangle2) #mask
+
+# Calculate histogram with mask and without mask
+# Check third argument for mask
+color = ('r','g','b')
+for i,col in enumerate(color):
+    histr2 = cv2.calcHist([img2],[i],mask2,[256],[0,256])
+    plt.subplot(326), plt.plot(histr2,color = col)
+    plt.xlim([0,256])
+
     
 #hist_full = cv2.calcHist([img2],[0],None,[256],[0,256])
 
-plt.subplot(221), plt.imshow(img2, 'gray')
-plt.subplot(222), plt.imshow(mask,'gray')
-plt.subplot(223), plt.imshow(masked_img, 'gray')
-
+plt.subplot(321), plt.imshow(img2)
+plt.subplot(322), plt.imshow(mask,'gray')
+plt.subplot(323), plt.imshow(masked_img, 'gray')
+plt.subplot(325), plt.imshow(masked_img2, 'gray')
 plt.show()
